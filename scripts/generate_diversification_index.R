b3f2bbc43c0dc93f1e2c355784c1ce418e81bf96
abigailgolden-noaa <abigail.golden@noaa.gov>
2025-01-02 16:39:34 -0800

    copied setup code for diversification indices from `01_create_networks_cciea.Rmd`

--- scripts/generate_diversification_index.R
+++ scripts/generate_diversification_index.R
@@ -2,4 +2,4 @@
 #' 
 #' Generate fishery diversification index based on Holland and Kasperski 2025 (CJFAS)
 #' Can output indices either at the individual vessel level coastwide (= all fish tickets associated with a particular vessel in a given year)
-#' Or at the port level (= all fish tickets landed in a given IOPAC port group in a given year)
+#' Or at the port level (= all fish tickets associated with a particular vessel and landed in a given IOPAC port group in a given year)
