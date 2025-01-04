3bd092fbb37ce46ad76a7f806d8b53bd83b744fd
abigailgolden-noaa <abigail.golden@noaa.gov>
2025-01-03 16:39:32 -0800

    calculated diversification index for a given year; next step, turn this code into a function

--- R/generate_fishery_matrix.R
+++ R/generate_fishery_matrix.R
@@ -7,17 +7,13 @@
 #' @param matrix_scale whether to construct a matrix for the entire coast (`"coastwide"`) or specific port (`"port"`)
 #' @param pcid_choose specify an IOPAC port group (only needs to be specified if `matrix_scale == "port`; will produce an error otherwise)
 #' @param year_choose Specify a year
-#' @param filter_for_conf use the `min_vessels` and `min_contribution` objects to filter the data for confidentiality
-#' @param min_vessels the minimum number of vessels participating in a fishery for that fishery to be retained in the network
-#' @param min_contribution the minimum contribution (as a proportion) to total exvessel revenue across all vessels for a fishery to be retained in the network
 #' @param min_rev the minimum revenue (in dollars) generated from all fisheries for a given vessel in a given year
 #' @param min_rev_indiv the minimum revenue (in dollars) generated from any one fishery for a given vessel in a given year
-#' @return non-confidential matrix of fishery-specific revenue in a given year, where each row is a vessel,
+#' @return matrix of fishery-specific revenue in a given year, where each row is a vessel,
 #' each column is a species grouping, and values are revenue
 
 
 revenue_matrix <- function(tickets, matrix_scale = "coastwide", pcid_choose = NA, year_choose = NA,
-                           filter_for_conf = TRUE, min_vessels = 3, min_contribution = 0.1,
                            min_rev = 1, min_rev_indiv = 1){
   
   # make sure that the appropriate inputs are included
@@ -67,18 +63,6 @@ revenue_matrix <- function(tickets, matrix_scale = "coastwide", pcid_choose = NA
   
   
   ##### for each vessel remove fisheries with below min_rev_indiv #####
-  # for each boat, set filtered_boats table value as "NA" for fisheries that don't generate at least min_rev_indiv in revenue annually.
-  boats_mat <- as.matrix(boats)
-  boats_mat[which(boats_mat<min_rev_indiv)] <- NA
-  filtered_boats <- as.data.frame(boats_mat, row.names=rownames(boats_mat), col.names=colnames(boats_mat))
-  
-  ##### calculate percent contributions, then remove fisheries with below min_rev_indiv #####
-  # make a new df with annual % revenue from each metier for each boat
-  percent_boats <- boats/rowSums(boats, na.rm = T)
-  percent_boats_mat <- as.matrix(percent_boats)
-  # for each boat, set percent_boats table value as "NA" for fisheries that don't generate at least min_rev_indiv in revenue annually. added 08192021
-  percent_boats_mat[which(boats<min_rev_indiv)] <- NA
-  percent_boats <- as.data.frame(percent_boats_mat, row.names=rownames(percent_boats_mat), col.names=colnames(percent_boats_mat))
   # for each boat, set filtered_boats table value as "NA" for fisheries that don't generate at least min_rev_indiv in revenue annually.
   boats_mat <- as.matrix(boats)
   boats_mat[which(boats_mat<min_rev_indiv)] <- NA
